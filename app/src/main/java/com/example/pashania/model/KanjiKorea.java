075df480fa8f058e5af58d9b52b5b153a44a3c01
RobertJif <roberttjjif@gmail.com>
2020-04-26 04:39:16 +0700

    Korean Extractor method done, not yet tested, waiting for front end

--- app/src/main/java/com/example/pashania/model/KanjiKorea.java
+++ app/src/main/java/com/example/pashania/model/KanjiKorea.java
@@ -1,4 +1,52 @@
 package com.example.pashania.model;
 
+import android.graphics.Bitmap;
+
 public class KanjiKorea {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKanji() {
+        return kanji;
+    }
+
+    public void setKanji(String kanji) {
+        this.kanji = kanji;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    private int id;
+    private String name;
+    private String kanji;
+    private String meaning;
+    private Bitmap image;
+
 }
--- app/src/main/java/com/example/pashania/service_layer/MyImageExtractor.java
+++ app/src/main/java/com/example/pashania/service_layer/MyImageExtractor.java
@@ -1,14 +1,81 @@
 package com.example.pashania.service_layer;
-
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import android.graphics.Bitmap;
-
 import com.example.pashania.model.KanjiKorea;
 
+import java.util.Arrays;
+
+import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
+import static org.opencv.imgproc.Imgproc.cvtColor;
+import static org.opencv.imgproc.Imgproc.erode;
+import static org.opencv.imgproc.Imgproc.resize;
+import static org.opencv.imgproc.Imgproc.threshold;
+
 public class MyImageExtractor {
+    //Configurations adjust this to your liking
+    private final int erodeAmount = 1;
+    private final int binaryTreshold = 127;
+    private final int imageSize = 50;
+
+    //Do not change this
+    private final Mat kernel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT,
+            new Size(2 * erodeAmount + 1, 2 * erodeAmount + 1),
+            new Point(erodeAmount, erodeAmount));
+
     public MyImageExtractor(){}
 
-    public KanjiKorea FindKoreanWord(Bitmap input, KanjiKorea[] dataTraining){
+    public KanjiKorea FindKoreanWord(Bitmap input, KanjiKorea[] data){
+        Mat test = Preprocess(input);
 
-        return null;
+        double smallestDistance = 0;
+        int smallestDistanceIndex = 0;
+
+        for (int i = 0; i < data.length ; i++){
+            Mat trained = Preprocess(data[i].getImage());
+
+            double[] result = new double[test.width()*test.height()];
+
+            //Find the smallest distance
+            for (int x = 0; x < test.width(); x++){
+                for (int y = 0; y < test.height(); y++){
+                result[i] = 0;
+                result[i] += Math.pow((test.get(y, x)[0] - trained.get(y, x)[0]), 2);
+                }
+            }
+
+            //Compare to get the smallest distance
+            if (smallestDistance > Math.sqrt(result[i])){
+                smallestDistance = Math.sqrt(result[i]);
+                smallestDistanceIndex = i;
+            }
+        }
+
+        return data[smallestDistanceIndex];
+    }
+
+    private Mat Preprocess(Bitmap input){
+        Mat result = new Mat();
+        Size imgBaseSize = new Size(imageSize,imageSize);
+
+        //convert java bitmap to openCV mat
+        Utils.bitmapToMat(input.copy(Bitmap.Config.ARGB_8888, true), result);
+
+        //resize to size we want
+        resize(result, result, imgBaseSize );
+
+        //transform to gray scale image
+        cvtColor(result, result, Imgproc.COLOR_RGB2GRAY);
+
+        //transform image to binary
+        threshold(result, result, binaryTreshold, 255, THRESH_BINARY);
+
+        //erode image
+        erode(result, result, kernel);
+
+        return result;
     }
 }
