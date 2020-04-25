11207d32d7584a8267821f2bd93c17e48aadc5c9
RobertJif <roberttjjif@gmail.com>
2020-04-26 02:49:30 +0700

    configure openCV

---
+++ app/src/main/java/com/example/pashania/model/KanjiKorea.java
@@ -0,0 +1,4 @@
+package com.example.pashania.model;
+
+public class KanjiKorea {
+}
---
+++ app/src/main/java/com/example/pashania/service_layer/MyImageExtractor.java
@@ -0,0 +1,14 @@
+package com.example.pashania.service_layer;
+
+import android.graphics.Bitmap;
+
+import com.example.pashania.model.KanjiKorea;
+
+public class MyImageExtractor {
+    public MyImageExtractor(){}
+
+    public KanjiKorea FindKoreanWord(Bitmap input, KanjiKorea[] dataTraining){
+
+        return null;
+    }
+}
